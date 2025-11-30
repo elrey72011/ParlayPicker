f6a06ac44157cb66c32c088e4ee6a4c7b2764734
elrey72011 <robert.a.velarde@gmail.com>
2025-11-30 11:57:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9625,6 +9625,8 @@ if is_vertex_ai_enabled():
                 selected_sports = []
                 if 'NCAAF' in st.session_state.get('selected_sports', []):
                     selected_sports.append('americanfootball_ncaaf')
+                if 'NFL' in st.session_state.get('selected_sports', []):
+                    selected_sports.append('americanfootball_nfl')
                 if 'NBA' in st.session_state.get('selected_sports', []):
                     selected_sports.append('basketball_nba')
                 if 'NCAAB' in st.session_state.get('selected_sports', []):
@@ -9633,7 +9635,7 @@ if is_vertex_ai_enabled():
                     selected_sports.append('icehockey_nhl')
                 
                 if not selected_sports:
-                    selected_sports = ['basketball_nba', 'americanfootball_ncaaf', 'basketball_ncaab', 'icehockey_nhl']
+                    selected_sports = ['basketball_nba', 'americanfootball_nfl', 'americanfootball_ncaaf', 'basketball_ncaab', 'icehockey_nhl']
                 
                 all_games = []
 
