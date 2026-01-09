b235b101011f6d4bb7a30252d0f8139e6df235e8
elrey72011 <robert.a.velarde@gmail.com>
2026-01-09 16:53:15 -0500

    Refactor away stats calculations in feature_processing
    Removed redundant calculation of away last 5 win percentage and adjusted related stats.

--- app_core/feature_processing.py
+++ app_core/feature_processing.py
@@ -1140,19 +1140,13 @@ def enrich_with_model_features(df: pd.DataFrame, api_clients: Dict[str, Any], se
         # Away Stats (use matched names)
         features_data['feature_away_win_pct'] = map_stat(away_matched_names, 'win_pct', default_win_pct)
         features_data['feature_away_away_win_pct'] = map_stat(away_matched_names, 'away_win_pct', default_win_pct)
-        features_data['feature_away_last5_win_pct'] = map_stat(away_matched_names, 'last5_win_pct', default_last5)
-
-        features_data['feature_away_ppg'] = map_stat(away_norm, 'points_per_game', default_ppg)
-        features_data['feature_away_oppg'] = map_stat(away_norm, 'points_allowed_per_game', default_oppg)
+        features_data['feature_away_last5_win_pct'] = map_stat(away_matched_names, 'last5_win_pct', default_last5)      
 
         # SCALING: NHL stats are ~3.0, model expects ~110.0. Scale by 35x if league is NHL.
         if is_nhl.any():
             nhl_scale_factor = 35.0
             features_data['feature_away_ppg'] = features_data['feature_away_ppg'].mask(is_nhl, features_data['feature_away_ppg'] * nhl_scale_factor)
             features_data['feature_away_oppg'] = features_data['feature_away_oppg'].mask(is_nhl, features_data['feature_away_oppg'] * nhl_scale_factor)
-
-        features_data['feature_away_streak'] = map_stat(away_norm, 'streak', pd.Series(0.0, index=df.index))
-        features_data['feature_away_turnovers'] = map_stat(away_norm, 'turnovers', pd.Series(0.0, index=df.index))
 
     # 4. Fill Defaults if stats_df was empty
     if 'feature_home_win_pct' not in features_data:
